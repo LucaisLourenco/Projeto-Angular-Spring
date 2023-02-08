995c5a465905a2dca1690b4cb7dddc1316b87958
Lucas Lourenço <lucaspeixe2018@gmail.com>
2023-02-08 00:49:24 -0300

    get-responsavel

--- backend-spring/src/main/java/com/lucas/backendspring/controllers/ResponsavelController.java
+++ backend-spring/src/main/java/com/lucas/backendspring/controllers/ResponsavelController.java
@@ -2,8 +2,14 @@ package com.lucas.backendspring.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.backendspring.models.Responsavel;
@@ -22,5 +28,18 @@ public class ResponsavelController {
     public List<Responsavel> list() {
         return responsavelRepository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Responsavel create(@RequestBody Responsavel responsavel) {
+        return responsavelRepository.save(responsavel);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Responsavel> findById(@PathVariable Long id) {
+        return responsavelRepository.findById(id)
+        .map(responsavel -> ResponseEntity.ok(responsavel))
+        .orElse(ResponseEntity.notFound().build());
+    } 
 
 }
